5f5cc10cdb108a0d2c00a7c89a52456a0efc3fb2
jozinho22 <josselin.douineau.1987@gmail.com>
2026-04-24 22:24:46 +0200

    1ère MEP

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',      // <--- Indispensable pour GitHub Pages
+  images: {
+    unoptimized: true,   // GitHub Pages ne supporte pas l'optimisation d'image native de Next
+  },
+  // Si ton projet est sur https://ton-nom.github.io/ton-depot/
+  // décommente et ajuste la ligne suivante :
+  // basePath: '/ton-nom-de-depot', 
 };
 
 export default nextConfig;
